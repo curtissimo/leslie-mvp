7060f2d09a1910acbf286ad17318b5d9ba12bc94
Curtis Schlak <realistschuckle@gmail.com>
2014-07-26 09:55:10 -0500

    Bring the gulp file up to parity with the grunt file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
-var docco, gulp;
+var docco, gulp, jslint, nodeunit;
 
 docco = require('gulp-docco');
 gulp = require('gulp');
-
-gulp.task('default', function () {
-
-});
+jslint = require('gulp-jslint');
+nodeunit = require('gulp-nodeunit');
 
 gulp.task('docs', function () {
-  gulp.src('./leslie.js')
+  'use strict';
+  return gulp
+    .src('./leslie.js')
     .pipe(docco())
     .pipe(gulp.dest('./docs/'));
 });
+
+gulp.task('test', function () {
+  'use strict';
+  return gulp
+    .src('./tests/*.js')
+    .pipe(nodeunit());
+});
+
+gulp.task('lint', function () {
+  'use strict';
+  return gulp
+    .src(['./leslie.js', './gulpfile.js'])
+    .pipe(jslint({ indent: 2, node: true }));
+});
+
+gulp.task('watch', function () {
+  'use strict';
+  gulp.watch('./leslie.js', [ 'test', 'lint' ]);
+  gulp.watch('./gulpfile.js', [ 'lint' ]);
+});
+
+gulp.task('dev', ['test', 'lint', 'watch']);
